5dc2129d7fe67495a671eef5bba7a3cfca594e3a
kristina <kristina.anicic1@gmail.com>
2025-11-05 05:38:47 +0100

    add plant repository

---
+++ Backend/PlantApp.Domain/Interfaces/Repository/IPlantRepository.cs
@@ -0,0 +1,9 @@
+﻿using PlantApp.Data.Models;
+using PlantApp.Domain.Dtos.Plant;
+
+namespace PlantApp.Domain.Interfaces.Repository;
+
+public interface IPlantRepository : IRepository<Plant>
+{
+    public Task<List<Plant>> GetPlantsFiltered(FilterByDto filter, string? name = null);
+}
---
+++ Backend/PlantApp.Domain/Repositories/PlantRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlantApp.Data;
+using PlantApp.Data.Models;
+using PlantApp.Domain.Dtos.Plant;
+using PlantApp.Domain.Interfaces.Repository;
+
+namespace PlantApp.Domain.Repositories;
+
+public class PlantRepository(AppDbContext context) : Repository<Plant>(context), IPlantRepository
+{
+    public async Task<List<Plant>> GetPlantsFiltered(FilterByDto filter, string? name = null)
+    {
+        return await dbSet.Where(p =>
+            (filter.IsLowMaintenance == null || p.IsLowMaintenance == filter.IsLowMaintenance) &&
+            (filter.IsDroughtResistant == null || p.IsDroughtResistance == filter.IsDroughtResistant) &&
+            (filter.Habits == null || p.Habits.Any(h => filter.Habits.Contains(h.Id))) &&
+            (filter.SoilType == null || p.SoilTypes.Any(s => filter.SoilType.Contains(s.Id))) &&
+            (filter.Spread == null || p.SpreadTypeId == filter.Spread) &&
+            (filter.Height == null || p.HeightTypeId == filter.Height) &&
+            (filter.TimeToFullHeight == null || p.TimeToFullHeightId == filter.TimeToFullHeight) &&
+            (filter.Exposure == null || p.Exposures.Any(e => e.Id == filter.Exposure)) &&
+            (string.IsNullOrEmpty(name) || 
+                EF.Functions.ILike(p.CommonName, $"%{name}%") || 
+                EF.Functions.ILike(p.BotanicalName, $"%{name}%"))
+            )
+            .ToListAsync();
+    }
+}
