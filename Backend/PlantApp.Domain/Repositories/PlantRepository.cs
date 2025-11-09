173dae739266fac277d73c8681f1d641c27eeb6a
kristina <kristina.anicic1@gmail.com>
2025-11-09 03:05:35 +0100

    add function for filtering plants

--- Backend/PlantApp.Domain/Repositories/PlantRepository.cs
+++ Backend/PlantApp.Domain/Repositories/PlantRepository.cs
@@ -12,7 +12,7 @@ public class PlantRepository(AppDbContext context) : Repository<Plant>(context),
     {
         return await dbSet.Where(p =>
             (filter.IsLowMaintenance == null || p.IsLowMaintenance == filter.IsLowMaintenance) &&
-            (filter.IsDroughtResistant == null || p.IsDroughtResistance == filter.IsDroughtResistant) &&
+            (filter.IsDroughtResistant == null || p.IsDroughtResistant == filter.IsDroughtResistant) &&
             (filter.Habits == null || p.Habits.Any(h => filter.Habits.Contains(h.Id))) &&
             (filter.SoilType == null || p.SoilTypes.Any(s => filter.SoilType.Contains(s.Id))) &&
             (filter.Spread == null || p.SpreadTypeId == filter.Spread) &&
@@ -21,8 +21,8 @@ public class PlantRepository(AppDbContext context) : Repository<Plant>(context),
             (filter.Exposure == null || p.Exposures.Any(e => e.Id == filter.Exposure)) &&
             (string.IsNullOrEmpty(name) || 
                 EF.Functions.ILike(p.CommonName, $"%{name}%") || 
-                EF.Functions.ILike(p.BotanicalName, $"%{name}%"))
-            )
-            .ToListAsync();
+                EF.Functions.ILike(p.BotanicalName, $"%{name}%")
+            ) && p.DeletedAt == null
+        ).ToListAsync();
     }
 }
