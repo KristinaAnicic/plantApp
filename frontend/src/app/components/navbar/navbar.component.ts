2b36cd90afa5ccaba52b7e43517b04f1d95f7609
kristina <kristina.anicic1@gmail.com>
2026-01-14 21:14:43 +0100

    fix navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,26 +1,44 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { LoginModal } from '../login-modal/login-modal';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [LoginModal],
+  imports: [LoginModal, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class NavbarComponent implements OnInit {
-  private authService = inject(AuthService);
-
-  isLoggedIn: boolean = false;
+export class NavbarComponent {
+  public authService = inject(AuthService);
+  
+  isUserMenuOpen = signal(false);
   isLoginOpen: boolean = false;
-
-  ngOnInit(): void {
-    this.isLoggedIn = this.authService.isUserLoggedIn();
-  }
+  isMobileMenuOpen = signal(false);
 
   toggleLoginModal() {
     this.isLoginOpen = !this.isLoginOpen;
+  }
+
+  toggleMenu() {
+    this.isMobileMenuOpen.update(val => !val);
+  }
+
+  closeMenu() {
+    this.isMobileMenuOpen.set(false);
+  }
+
+  signOut(){   
+    this.authService.logout().subscribe({
+      next: () => {
+        this.isUserMenuOpen.set(false);
+      },
+      error: (err) => {
+        console.error('Logout failed', err);
+        this.isUserMenuOpen.set(false);
+      }
+    });  
   }
   
 }
