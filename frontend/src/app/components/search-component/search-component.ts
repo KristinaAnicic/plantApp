ebc040a9ddd4f323ffef83ec698dad342e6a1a3e
kristina <kristina.anicic1@gmail.com>
2026-01-24 02:32:27 +0100

    make search component more compatible

--- frontend/src/app/components/search-component/search-component.ts
+++ frontend/src/app/components/search-component/search-component.ts
@@ -8,11 +8,12 @@ import { Component, effect, EventEmitter, input, OnInit, output, Output, signal,
   styleUrl: './search-component.css',
 })
 export class SearchComponent {
-  @Output() search = new EventEmitter<void>();
-
+  search = output<void>();
   searchTriggered = output<string>();
   currentText = input<string>('');
   searchQuery = signal<string>('');
+  placeholder = input<string>("Search...");
+  iconType = input<'search' | 'location'>('search');
   
   constructor() {
     effect(() => {
