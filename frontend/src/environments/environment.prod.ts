36b35822819d428bc5e28c0452dd9ccbdb352786
kristina <kristina.anicic1@gmail.com>
2026-05-27 02:29:08 +0200

    configure production API URL for deployment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: '/api',
+    production: true,
+    apiUrl: 'https://plant-app-auh2anf9fsf5cyc9.westeurope-01.azurewebsites.net',
     appwriteEndpoint: 'https://fra.cloud.appwrite.io/v1',
     appwriteProjectId: '69711305002eed3e64c0',
     appwriteProjectName: 'PlantApp',
